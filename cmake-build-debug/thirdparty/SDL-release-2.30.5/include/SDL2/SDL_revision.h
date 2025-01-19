5ad8c876dff02bbcf3fad82c128d32f4425de202
Daniel Leger <82476403+blaidd-ddrwg613@users.noreply.github.com>
2025-01-19 02:16:42 -0500

    Delete cmake-build-debug directory

--- cmake-build-debug/thirdparty/SDL-release-2.30.5/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.30.5-g3dae3ec (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-2.30.5-g3dae3ec"
-#endif
